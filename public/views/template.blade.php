fa8266b517881a01b36c478325ce2c06b2270155
Clément Hémidy <clemh78@gmail.com>
2021-06-07 21:23:45 +0200

    spell error on UEFA

--- public/views/template.blade.php
+++ public/views/template.blade.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
 <head>
-    <title>Kupi.fr | EUFA EURO 2020</title>
+    <title>Kupi.fr | UEFA EURO 2020</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
     <!-- BOWER COMPONENTS -->
